870008f7c12c2b0fcdc6d7ac9f22fa73ecca9ddf
Wayne Starr (Racer159) <racer159@live.com>
2015-04-04 11:54:17 -0600

    Improved interactivity for users marked as professors

--- app/static/js/homeController.js
+++ app/static/js/homeController.js
@@ -14,13 +14,12 @@ app.controller('HomeController', function($http){
 	
 
 	self.courses = [{"name":"Eng of Enterprise SW Systems", "projects":[
-		{"name":"Main Project", "notifications":["3 Unread Messages",
-		"Deliverable 1 due 4/7/2015"], "team":"Group 4"},
-		{"name":"Research Project", "notifications":["1 Unread Message",
-		"Research Paper due 5/10/2015"], "team":"Group 3"}]},
+		{"id":"1", "name":"Main Project", "notifications":["3 Unread Messages",
+		"Deliverable 1 due 4/7/2015", "3 New Tasks Assigned"], "team":"Group 4"},
+		{"id":"2", "name":"Research Project", "notifications":["Research Paper due 5/10/2015"], 
+		"team":"Group 3"}]},
 		{"name":"SW Process and Project Manage", "projects":[
-		{"name":"PM Project", "notifications":["SDP Revision 1 due 4/5/2015",
-		"3 New Tasks Assigned"], "team":"Group 1"}]}
+		{"id":"3", "name":"PM Project", "notifications":[], "team":"Group 1"}]}
 		];
 
 	self.init = function(){
@@ -34,6 +33,10 @@ app.controller('HomeController', function($http){
 			self.searchUrl = data.classSearch
 			self.loaded = true;
 		});
+	}
+
+	self.gotoProject = function(id) {
+		alert(id);
 	}
 	
 	self.init();
