9d669fe3717259a99160bafb2e0aa3f6e11fca75
Wayne Starr (Racer159) <racer159@live.com>
2015-04-04 12:29:19 -0600

    Enabled 'creation' and 'removal' of projects

--- app/static/js/homeController.js
+++ app/static/js/homeController.js
@@ -4,7 +4,7 @@
 
 var app = angular.module('minerva');
 
-app.controller('HomeController', function($http){
+app.controller('HomeController', function($http, $location){
 	
 	console.log("Created Home Controller");
 	
@@ -15,12 +15,11 @@ app.controller('HomeController', function($http){
 
 	self.courses = [{"name":"Eng of Enterprise SW Systems", "projects":[
 		{"id":"1", "name":"Main Project", "notifications":["3 Unread Messages",
-		"Deliverable 1 due 4/7/2015", "3 New Tasks Assigned"], "team":"Group 4"},
+		"Deliverable 1 due 4/7/2015", "3 New Tasks Assigned"], "team":"Group 4", "numteams":8},
 		{"id":"2", "name":"Research Project", "notifications":["Research Paper due 5/10/2015"], 
-		"team":"Group 3"}]},
+		"team":"Group 3", "numteams":5}]},
 		{"name":"SW Process and Project Manage", "projects":[
-		{"id":"3", "name":"PM Project", "notifications":[], "team":"Group 1"}]}
-		];
+		{"id":"3", "name":"PM Project", "notifications":[], "team":"Group 1", "numteams":13}]}];
 
 	self.init = function(){
 		console.log("Making HTTP Request");
@@ -36,7 +35,25 @@ app.controller('HomeController', function($http){
 	}
 
 	self.gotoProject = function(id) {
-		alert(id);
+		$location.search('id', id);
+		//$location.path('/project');
+	}
+
+	self.addProject = function(course) {
+		course.addProject = false;
+		course.projects.push({'name':course.addProjectName, 'description':course.addProjectDesc,
+			'team':'No Team Assigned', 'numteams':0, 'notifications':[]});
+		course.addProjectName = '';
+		course.addProjectDesc = '';
+	}
+
+	self.removeProject = function(course, project) {
+		for (var i = 0; i < course.projects.length; i++) {
+			if (course.projects[i] == project) {
+				course.projects.splice(i, 1);
+				break;
+			}
+		}
 	}
 	
 	self.init();
