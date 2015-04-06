a262ac07b57bf8749334b1cccbc5e0186d64bafa
Chris Enoch <majorpayne327@gmail.com>
2015-04-06 18:14:32 -0400

    Cleaned up user classes

--- app/database/User.py
+++ app/database/User.py
@@ -6,7 +6,7 @@ Created on Apr 6, 2015
 
 import mongo_client
 
-class User:
+class User(object):
     id=None
     name=None
     email=None
@@ -15,55 +15,57 @@ class User:
     def to_hashmap(self):
         return self.__dict__
 
-
-class Student(User):
-    team_id = None
-    task_ids = None
-    message_ids = None
-        
-    def __init__(self,id,name,email):
-        self.id = id
+    def __init__(self, uid, name, email, message_ids, course_id):
+        self.uid = uid
         self.name = name
         self.email = email
-        self.team_ids = []
-        self.task_ids = []
-        self.message_ids = []
+        self.message_ids = message_ids
+        self.course_id = id
+
+
+class Student(User):
+
+    def __init__(self, uid, name, email, team_ids=[], task_ids=[], message_ids=[]):
+        super(self.__class__, self).__init__(uid, name, email, message_ids)
+        self.team_ids = team_ids
+        self.task_ids = task_ids
         self.save()
 
-    def add_task(self,id):
-        self.task_ids.insert(0,id)
+    def add_task(self, uid):
+        self.task_ids.insert(0, uid)
         self.save()
         
-    def add_message(self,id):
-        self.message_ids.insert(0,id)
+    def add_message(self, uid):
+        self.message_ids.insert(0, uid)
         self.save()
         
     def save(self):
-        mongo_client.update('students',{'id': self.id},self.to_hashmap())
-    
+        mongo_client.update('users',{'id': self.id},self.to_hashmap())
+
     @staticmethod  
-    def get_student(id):
-        student_map = mongo_client.get_from('students',id)
-        if student_map != None:
-			student_object = Student(student_map['id'],student_map['name'],student_map['email'])
-			student_object.team_ids = student_map['team_ids']
-			student_object.task_ids = student_map['task_ids']
-			student_object.message_ids = student_map['message_ids']
-			return student_object
+    def get_student(uid):
+        user_map = mongo_client.get_from('users',uid)
+        if user_map != None:
+            student_object = Student(user_map['uid'], user_map['name'], user_map['email'], 
+                                     user_map['team_ids'], user_map['task_ids'], user_map['message_ids'])
+            return student_object
 
 
 class Instructor(User):
     class_id = None
     
-    def __init__(self,id,uid,name,email):
-        self.id = id
-        self.uid = uid
-        self.name = name
-        self.email = email
-        self.class_id = []
+    def __init__(self, uid, name, email, message_ids=[]):
+        super(self.__class__, self).__init__(uid, name, email, message_ids)
         
     def add_class(self,class_id):
         self.class_id += [class_id]
         
     def save(self):
-        mongo_client.update('instructors',{'id': self.id},self.to_hashmap())
+        mongo_client.update('users',{'id': self.id},self.to_hashmap())
+        
+    @staticmethod  
+    def get_instructor(uid):
+        user_map = mongo_client.get_from('users',id)
+        if user_map != None:
+            user_object = Instructor(user_map['id'],user_map['name'],user_map['email'],user_map['message_ids'])
+            return user_object
