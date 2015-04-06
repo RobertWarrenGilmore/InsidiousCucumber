005d9d48e61063bbd2320e187f2fb4166f0a7991
Chris Enoch <majorpayne327@gmail.com>
2015-04-06 15:52:09 -0400

    Fixed Python Logger
    - Logger will create a new log file
    - Changed FileHandler to Rotating FileHandler
        - When file size (1kb) is reached a new file will be created
        - Keeps a 3 file history

--- app/extras/logger/__init__.py
+++ app/extras/logger/__init__.py
@@ -1,15 +1,18 @@
-import logging, sys
+import sys
+from logging import getLogger, StreamHandler, Formatter, WARNING
+from logging.handlers import RotatingFileHandler
 
+LOG_FILENAME = 'logs/flask.log'
 
-def initLogging(loggerName,loglevel=logging.WARNING):
+def initLogging(loggerName,loglevel=WARNING):
    
-    my_logger = logging.getLogger(loggerName)
+    my_logger = getLogger(loggerName)
     my_logger.setLevel(loglevel)
     
-    my_console_handler = logging.StreamHandler()
-    my_file_handler = logging.FileHandler('logs/flask.log')
+    my_console_handler = StreamHandler()
+    my_file_handler = RotatingFileHandler(LOG_FILENAME, mode='w', maxBytes=1000, backupCount=3)
     
-    my_formatter = logging.Formatter('%(asctime)s [%(levelname)s]: %(filename)s Line:%(lineno)d\n%(message)s')
+    my_formatter = Formatter('%(asctime)s [%(levelname)s]: %(filename)s Line:%(lineno)d\n%(message)s')
     
     my_console_handler.setFormatter(my_formatter)
     my_file_handler.setFormatter(my_formatter)
