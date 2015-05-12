7269175d90e75e1a81596f47e800b4957207cc0a
RGood <mullan17@gmail.com>
2015-05-12 00:14:38 -0400

    Added update functionality for projects

--- app/views/api/project_api.py
+++ app/views/api/project_api.py
@@ -17,3 +17,15 @@ def make_project(args):
 	project = Project.init_project(args['name'],args['url'],args['desc'])
 	project.save()
 	return Response(status=201)
+	
+def update_project(id,args):
+	project = Project.objects(pid=id).first()
+	if project is not None:
+		project.name = args['name']
+		project.url = args['url']
+		project.descr = args['description']
+		project.team_ids = args['team_ids']
+		project.deliverable_ids = args['deliverable_ids']
+		project.save()
+		return Response(status=201)
+	return Response(status=404)
--- tests/test_api/test_project_api.py
+++ tests/test_api/test_project_api.py
@@ -41,8 +41,18 @@ class TestUserApi(flask_testing.TestCase):
 	def test_post_project(self):
 		with self.client as c:
 			response = c.post('/project',data={'name':'Test Proj','url':'http://www.example.com','desc':'Test Description'})
-			print(response)
 		self.assertStatus(response,201,"Incorrect response code")
-	11
+		
+	def test_update_project(self):
+		with self.client as c:
+			data = {}
+			data['name'] = 'Update Name'
+			data['url'] = 'http://example.com'
+			data['desc'] = 'Update Description'
+			data['team_ids'] = [1,2,3]
+			data['deliverable_ids'] = [1,2,3,4]
+			response = c.put('/project/'+str(self.proj.pid),data=data)
+		self.assertStatus(response,201,"Incorrect status code")
+		
 if __name__ == "__main__":
     nose.main()
